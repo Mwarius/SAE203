7708df18b8c464170e5f7e9009c6f9a3aac8d06f
VaIkorion <meven.lesaulnier@gmail.com>
2025-06-10 22:18:21 +0200

    Update wiki.php

--- Intranet/wiki.php
+++ Intranet/wiki.php
@@ -16,33 +16,37 @@ echo "<!DOCTYPE html>
 <body class='d-flex flex-column min-vh-100'>
 <header>
   <div>
-    <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <div class='container-fluid'>
-        <a class='navbar-brand' href='./accueil_intranet.php'>GMG</a>
-        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'>
-          <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse' id='navbarNav'>
-          <ul class='navbar-nav me-auto'>
-            <li class='nav-item'>
-              <a class='nav-link text-light' href='./annuaire.php'>Annuaire</a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link text-light' href='./gestion_fichier.php'>Gestion fichier</a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link text-light active' href='./wiki.php'>Wiki</a>
-            </li>
-          </ul>
-          <div class='d-flex align-items-center'>";
-            if (isset($_SESSION['prenom']) && isset($_SESSION['nom']) && isset($_SESSION['fonction'])) {
-              echo "<span class='text-light me-2'>Connecté en tant que ". htmlspecialchars($_SESSION['prenom']) ." ". htmlspecialchars($_SESSION['nom']) .", ". htmlspecialchars($_SESSION['fonction']) ."</span>";
-              echo "<a href='./portail_deconnexion.php' class='btn btn-outline-light btn-sm'>Se déconnecter</a>";
-            }
-          echo "</div>
+  <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+    <div class='container-fluid'>
+      <a class='navbar-brand' href='./accueil_intranet.php'>GMG</a>
+      <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'>
+        <span class='navbar-toggler-icon'></span>
+      </button>
+      <div class='collapse navbar-collapse' id='navbarNav'>
+        <ul class='navbar-nav me-auto'>
+          <li class='nav-item'>
+            <a class='nav-link text-light' href='./annuaire.php'>Annuaire</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link text-light' href='./gestion_fichier.php'>Gestion fichier</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link text-light' href='./wiki.php'>Wiki</a>
+          </li>
+          <li>
+            <a class='nav-link text-light' href='./profil.php'>Mon profil</a>
+          </li>
+        </ul>
+        <div class='d-flex align-items-center'>";
+          if (isset($_SESSION['prenom']) && isset($_SESSION['nom']) && isset($_SESSION['fonction'])) {
+            echo "<span class='text-light me-2'>Connecté en tant que ". htmlspecialchars($_SESSION['prenom']) . " ". htmlspecialchars($_SESSION['nom']) . ", ". htmlspecialchars($_SESSION['fonction']). "</span>";
+            echo "<a href='./portail_deconnexion.php' class='btn btn-outline-light btn-sm'>Se déconnecter</a>";
+          }
+        echo "
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
   </div>
   <div class='jumbotron jumbotron-fluid p-5 bg-primary text-white'>
     <div class='container'>
